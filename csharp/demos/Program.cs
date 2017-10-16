55fe063dcb6e31fc1a966ee89de7fc79359641f0
makiroggers <makiroggers@gmail.com>
2017-10-15 22:21:34 -0700

    Added loops.

--- csharp/demos/Program.cs
+++ csharp/demos/Program.cs
@@ -7,6 +7,14 @@ namespace demos
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
