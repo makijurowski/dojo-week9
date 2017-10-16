6615b16a106b2c4626b31d9e241d710bc1134a9b
makiroggers <makiroggers@gmail.com>
2017-10-16 16:36:17 -0700

    Updated instructions.

--- algorithms/recursion-telephoneWords.js
+++ algorithms/recursion-telephoneWords.js
@@ -1,8 +1,19 @@
 /* 
 RECURSION: Telephone Words (pg. 102)
 Given a 7-digit telephone number, return an array of all possible strings that equate
-to the phone number. 
+to the phone number. Mapping:
+
+0: O;
+1: I;
+2: ABC;
+3: DEF;
+4: GHI;
+5: JKL;
+6: MNO;
+7: PQRS;
+8: TUV;
+9: WXYZ;
 
 INPUT (str) => "818-2612"
-OUTPUT (arr of str) => 243 total combos > ["vitamic", "titania", ...]
+OUTPUT (arr of str) => 243 total combos including ["vitamic", "titania", ...]
 */
