b5648156cc24f53577545d15e9198417dc30f613
makiroggers <makiroggers@gmail.com>
2017-10-17 12:04:24 -0700

    Added human and card_deck programs.

---
+++ csharp/card_deck/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace card_deck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ csharp/human/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace human
+{
+    class Program
+    {
+        public class Human
+        {
+            public string Name { get; set; }
+            public int Strength { get; set; }
+            public int Intelligence { get; set; }
+            public int Dexterity { get; set; }
+            public int Health{ get; set; }
+
+            public Human(string name, int strength=3, int intelligence=3, int dexterity=3, int health=100) 
+            {
+                Name = name;
+                Strength = strength;
+                Intelligence = intelligence;
+                Dexterity = dexterity;
+                Health = health;
+            }
+
+            public void ShowStatus() {
+                System.Console.WriteLine("Name: {0}", Name);
+                System.Console.WriteLine("Strength: {0}", Strength);
+                System.Console.WriteLine("Intelligence: {0}", Intelligence);
+                System.Console.WriteLine("Dexterity: {0}", Dexterity);
+                System.Console.WriteLine("Health: {0}", Health);
+            }
+
+            public void Attack(Human human)
+            {
+                human.Health -= (this.Strength * 5);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Human me = new Human("Maki", 3, 10, 3, 200);
+            Human me = new Human("Maki", health: 200);
+            Human Lola = new Human("Lola");
+            System.Console.WriteLine(me);
+            System.Console.WriteLine("--------Before Attacking--------");
+            me.ShowStatus();
+            System.Console.WriteLine("--------------------------------");
+            Lola.ShowStatus();
+            System.Console.WriteLine("--------After Attacking---------");
+            me.Attack(Lola);
+            Lola.ShowStatus();
+            System.Console.WriteLine("--------------------------------");
+        }
+    }
+}
