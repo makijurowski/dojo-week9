a2096f05c6f3398c9f033c30a7c298723d58b5a3
makiroggers <makiroggers@gmail.com>
2017-10-16 20:50:28 -0700

    Added boxing assignment.

---
+++ csharp/boxing/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace boxing
+{
+    class Program
+    {
+        public static void Boxing()
+        {
+            int totalIntSum = 0;
+            List<object> RealEmptyList = new List<object>()
+            {
+                7,
+                28,
+                -1,
+                true,
+                "chair"
+            };
+            // foreach (object item in RealEmptyList.GetType().GetProperties())
+            foreach (object item in RealEmptyList)
+            {
+                long myLength = RealEmptyList.Count;
+                if (item is int) {
+                    System.Console.WriteLine(item);
+                    totalIntSum += Convert.ToInt32(item);
+                }
+                else if (item is string) {
+                    System.Console.WriteLine(item);
+                }
+                else if (item is bool) {
+                    System.Console.WriteLine(item);
+                }
+                else if (item is object) {
+                    System.Console.WriteLine(item);
+                }
+                else {
+                    System.Console.WriteLine("This did not work!");
+                }
+            }
+            System.Console.WriteLine("Total sum of all integers: {0}", totalIntSum);
+        }
+        static void Main(string[] args)
+        {
+            Boxing();
+        }
+    }
+}
--- csharp/demos/Program.cs
+++ csharp/demos/Program.cs
@@ -4,8 +4,29 @@ namespace demos
 {
     class Program
     {
+        public static void ExplicitCasting()
+        {   
+            // Working example
+            object ActuallyString = "a string";
+            string ExplicitString = ActuallyString as string;
+            // Non-working example
+            // object ActuallyInt = 256;
+            // int ExplicitInt = ActuallyInt as int;
+        }
+        public static void Boxing()
+        {
+            // Checking data types for use with boxing
+            object BoxedData = "This is clearly a string";
+            if (BoxedData is int) {
+                System.Console.WriteLine("We have an integer?");
+            }
+            else if (BoxedData is string) {
+                System.Console.WriteLine("It is totally a string!");
+            }
+        }
         static void Main(string[] args)
         {
+            Boxing();
             Console.WriteLine("Hello World!");
             for (int i = 1; i <= 5; i++)
             {
