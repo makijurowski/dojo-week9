7d2267d3add12adc7af34048fdcebfbdcf69abae
makiroggers <makiroggers@gmail.com>
2017-10-17 10:45:10 -0700

    Added solution and renamed alphabetical attempt inOrderSubsets.

--- algorithms/recursion-inOrderSubsets.js
+++ algorithms/recursion-inOrderSubsets.js
@@ -9,52 +9,20 @@ INPUT (str) => "abc"
 OUTPUT (arr) => ["", "a", "b", "c", "ab", "ac", "bc", "abc"]
 */
 
-
-function strSubsets(str, results=[], strArray=[]) {
-    if (results.length == 0) {
-        results.push("");
-        strArray = str.split("");
-        strSubsets(str, results, strArray);
-    }
-    else if (strArray.length == 0) {
-        return results;
-    }
-    else {
-        for (var n = 0; n < strArray.length; n++) {
-            for (var i = 0; i < results.length; i++) {
-                console.log("strArray: " + strArray + " and results: " + results);
-                stringLetter = strArray[n];
-                console.log("stringLetter: " + stringLetter);
-                resultsLetter = results[i];
-                console.log("resultsLetter: " + resultsLetter);
-                console.log("indexOf: " + results.indexOf(stringLetter));
-                if (!stringLetter) {
-                    return results;
-                }
-                else if (stringLetter.toLowerCase() != stringLetter) {
-                    strArray = strArray.slice(n + 1);
-                    console.log("stringLetter: " + stringLetter);
-                    console.log("first nested if of results: " + results);
-                    strSubsets(str, results, strArray);
-                }
-                else if (stringLetter.localeCompare.resultsLetter > 0) {
-                    results[i] = resultsLetter + stringLetter;
-                    console.log("second nested if of results: " + results);
-                }
-                else if (results.indexOf(stringLetter) === -1) {
-                    results.push(stringLetter);
-                    console.log("third nested if of results: " + results);
-                }
-                else {
-                    strArray = strArray.slice(n + 1);
-                    console.log("forth nested if of results: " + results);
-                    strSubsets(str, results, strArray);
-                }
-            }
-        }
-    }
-    console.log(results);
+function strSubsets(str, results = []) {
+  if (str.length === 0) {
+    results.push(str);
     return results;
+  }
+  var arr = str.split("");
+  arr.length--;
+  var str1 = arr.join("");
+  strSubsets(str1, results);
+  var endIndex = results.length;
+  for (let i = 0; i < endIndex; i++) {
+    results.push(results[i] + str.charAt(str.length - 1));
+  }
+  return results;
 }
 
 console.log(strSubsets("hello"));
