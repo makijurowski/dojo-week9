3e361d66fb8b34470b7200479ea1f00078604ee7
makiroggers <makiroggers@gmail.com>
2017-10-16 18:14:15 -0700

    Added solutions to problems involving arrays, lists, and dicts.

--- csharp/collections/Program.cs
+++ csharp/collections/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace collections
 {
@@ -16,41 +18,65 @@ namespace collections
 
         public static void MultiplicationTable()
         {
-            int[] oneToTen = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-            int[] newRow = new int[10];
-
-            for (int i = 0; i < 10; i++)
-            {
-                newRow[i] = i + 1;
-            }
-            
-            // foreach (int n in newRow)
-            // {
-            //     System.Console.WriteLine(n);
-            // }
-
             int cols = 10;
             int rows = 10;
-            int count = 1;
-            int[,] multTable = new int[rows, cols];
+            long[,] multTable = new long[rows, cols];
 
             for (int n = 0; n < rows; n++)
             {
-                System.Console.WriteLine(count);
                 for (int m = 0; m < cols; m++)
                 {
-                    multTable[n, m] = count + (m + 1);
-                    System.Console.WriteLine(multTable[n, m]);
+                    multTable[n, m] = (n + 1)*(m + 1);
+                    if (multTable[n, m] > 9)
+                    {
+                        Console.Write(string.Format("{0}   ", multTable[n, m]));
+                    }
+                    else 
+                    {
+                        Console.Write(string.Format(" {0}   ", multTable[n, m]));
+                    }
                 }
-                count++;
+                Console.Write(Environment.NewLine + Environment.NewLine);
             }
-
         }
+
+        public static List<string> ListOfFlavors()
+        {
+            List<string> iceCreamFlavors = new List<string>();
+            iceCreamFlavors.Add("Vanilla");
+            iceCreamFlavors.Add("Chocolate");
+            iceCreamFlavors.Add("Strawberry");
+            iceCreamFlavors.Add("Swirl");
+            iceCreamFlavors.Add("Lemon");
+            System.Console.WriteLine("Total flavors before delete: {0}", iceCreamFlavors.Count);
+            iceCreamFlavors.RemoveAt(2);
+            System.Console.WriteLine("Total flavors after delete: {0}", iceCreamFlavors.Count);
+            return iceCreamFlavors;
+        }
+
+        public static void UserInfoDict()
+        {
+            Dictionary<string, string> userInfo = new Dictionary<string, string>();
+            string[] names = {"Tim", "Martin", "Nikki", "Sara"};
+            Random random = new Random();
+            foreach (string name in names)
+            {
+                int randomNum = random.Next(0, names.Length);
+                userInfo.Add(name, ListOfFlavors()[randomNum]);
+            }
+            foreach (KeyValuePair<string, string> item in userInfo)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", item.Key, item.Value);
+            }
+        }
+
 
         static void Main(string[] args)
         {
             // BasicArrays();
-            MultiplicationTable();
+            // MultiplicationTable();
+            // ListOfFlavors();
+            // UserInfoDict();
         }
     }
 }
