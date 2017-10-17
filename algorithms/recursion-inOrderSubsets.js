319d3fd3a141d671f9fe325aceffb6321dc16566
makiroggers <makiroggers@gmail.com>
2017-10-17 07:16:48 -0700

    JAVASCRIPT GAAAAH

--- algorithms/recursion-inOrderSubsets.js
+++ algorithms/recursion-inOrderSubsets.js
@@ -1,7 +1,7 @@
 /* 
 RECURSION: In-Order Subsets (pg. 101)
 Create a strSubsets(str), return an array with every possible
-in-order character subset of str. The result array itself does not 
+in-order character subset of str. The results array itself does not 
 need to be in any specific order--it is the subset of letters in each 
 string that must be in the same order as they were in the original.
 
@@ -9,3 +9,52 @@ INPUT (str) => "abc"
 OUTPUT (arr) => ["", "a", "b", "c", "ab", "ac", "bc", "abc"]
 */
 
+
+function strSubsets(str, results=[], strArray=[]) {
+    if (results.length == 0) {
+        results.push("");
+        strArray = str.split("");
+        strSubsets(str, results, strArray);
+    }
+    else if (strArray.length == 0) {
+        return results;
+    }
+    else {
+        for (var n = 0; n < strArray.length; n++) {
+            for (var i = 0; i < results.length; i++) {
+                console.log("strArray: " + strArray + " and results: " + results);
+                stringLetter = strArray[n];
+                console.log("stringLetter: " + stringLetter);
+                resultsLetter = results[i];
+                console.log("resultsLetter: " + resultsLetter);
+                console.log("indexOf: " + results.indexOf(stringLetter));
+                if (!stringLetter) {
+                    return results;
+                }
+                else if (stringLetter.toLowerCase() != stringLetter) {
+                    strArray = strArray.slice(n + 1);
+                    console.log("stringLetter: " + stringLetter);
+                    console.log("first nested if of results: " + results);
+                    strSubsets(str, results, strArray);
+                }
+                else if (stringLetter.localeCompare.resultsLetter > 0) {
+                    results[i] = resultsLetter + stringLetter;
+                    console.log("second nested if of results: " + results);
+                }
+                else if (results.indexOf(stringLetter) === -1) {
+                    results.push(stringLetter);
+                    console.log("third nested if of results: " + results);
+                }
+                else {
+                    strArray = strArray.slice(n + 1);
+                    console.log("forth nested if of results: " + results);
+                    strSubsets(str, results, strArray);
+                }
+            }
+        }
+    }
+    console.log(results);
+    return results;
+}
+
+console.log(strSubsets("hello"));
--- algorithms/recursion-telephoneWords.js
+++ algorithms/recursion-telephoneWords.js
@@ -19,16 +19,16 @@ OUTPUT (arr of str) => 243 total combos including ["vitamic", "titania", ...]
 */
 
 var keyMappingObject = {
-    "0": ["O"],
-    "1": ["I"],
-    "2": ["A", "B", "C"],
-    "3": ["D", "E", "F"],
-    "4": ["G", "H", "I"],
-    "5": ["J", "K", "L"],
-    "6": ["M", "N", "O"],
-    "7": ["P", "Q", "R", "S"],
-    "8": ["T", "U", "V"],
-    "9": ["W", "X", "Y", "Z"],
+  "0": ["O"],
+  "1": ["I"],
+  "2": ["A", "B", "C"],
+  "3": ["D", "E", "F"],
+  "4": ["G", "H", "I"],
+  "5": ["J", "K", "L"],
+  "6": ["M", "N", "O"],
+  "7": ["P", "Q", "R", "S"],
+  "8": ["T", "U", "V"],
+  "9": ["W", "X", "Y", "Z"]
 };
 
 var keyMappingArray = [
@@ -41,49 +41,45 @@ var keyMappingArray = [
   ["M", "N", "O"],
   ["P", "Q", "R", "S"],
   ["T", "U", "V"],
-  ["W", "X", "Y", "Z"],
+  ["W", "X", "Y", "Z"]
 ];
 
 // Provided test case
 var testPhoneNum = "818-2612";
 // Format phone number as array of numbers
-testPhoneNum = testPhoneNum.slice(0, 3) + testPhoneNum.slice(3-7);
-testPhoneNum = testPhoneNum.split('');
+testPhoneNum = testPhoneNum.slice(0, 3) + testPhoneNum.slice(3 - 7);
+testPhoneNum = testPhoneNum.split("");
 
 console.log(testPhoneNum);
 
 // Declare variables for pointer and empty solution array
 var pointer = 0;
-var solution = [''];
+var solution = [""];
 
 function telephoneWords(testPhoneNum, solution, pointer) {
-    while (pointer < testPhoneNum.length) {
+  while (pointer < testPhoneNum.length) {
     // Find all possible values at pointer in the keymap
     result = keyMappingArray[testPhoneNum[pointer]];
     if (result.length == 1) {
+      for (let n = 0; n < solution.length; n++) {
+        solution[n].concat(result[0]);
+      }
+      // console.log(solution);
+    } else {
+      // Loop through possible values
+      for (let i = 0; i < result.length; i++) {
         for (let n = 0; n < solution.length; n++) {
-          solution[n].concat(result[0]);
+          solution[n].concat(result[i]);
         }
-        // console.log(solution);
-    }
-    else {
-        // Loop through possible values
-        for (let i = 0; i < result.length; i++) {
-            for (let n = 0; n < solution.length; n++) {
-                solution[n].concat(result[i]);
-            }
-            // solution.push(result[i]);
-            console.log(solution);
-        }
+        // solution.push(result[i]);
+        console.log(solution);
+      }
     }
     pointer++;
-    }
+  }
 }
 
 telephoneWords(testPhoneNum, solution, pointer);
-
-
-
 
 // console.log(testPhoneNum);
 // console.log(keyMappingArray);
