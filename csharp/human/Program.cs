404c96e971c301c633485c9ae2def538f933337d
makiroggers <makiroggers@gmail.com>
2017-10-18 09:14:49 -0700

    Added classes for wizard, ninja, and samurai.

--- csharp/human/Program.cs
+++ csharp/human/Program.cs
@@ -1,55 +1,224 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace human
 {
-    class Program
+    public class Human
     {
-        public class Human
+        public string Name { get; set; }
+        public int Strength { get; set; }
+        public int Intelligence { get; set; }
+        public int Dexterity { get; set; }
+        public int Health { get; set; }
+
+        // Constructor for Human class with default values added (only passes name as argument)
+        public Human(string name)
         {
-            public string Name { get; set; }
-            public int Strength { get; set; }
-            public int Intelligence { get; set; }
-            public int Dexterity { get; set; }
-            public int Health{ get; set; }
-
-            public Human(string name, int strength=3, int intelligence=3, int dexterity=3, int health=100) 
-            {
-                Name = name;
-                Strength = strength;
-                Intelligence = intelligence;
-                Dexterity = dexterity;
-                Health = health;
-            }
-
-            public void ShowStatus() {
-                System.Console.WriteLine("Name: {0}", Name);
-                System.Console.WriteLine("Strength: {0}", Strength);
-                System.Console.WriteLine("Intelligence: {0}", Intelligence);
-                System.Console.WriteLine("Dexterity: {0}", Dexterity);
-                System.Console.WriteLine("Health: {0}", Health);
-            }
-
-            public void Attack(Human human)
-            {
-                human.Health -= (this.Strength * 5);
-            }
+            Name = name;
+            Strength = 3;
+            Intelligence = 3;
+            Dexterity = 3;
+            Health = 100;
         }
 
+        // Constructor for Human class without default values (passes all stats as arguments)
+        public Human(string name, int strength, int intelligence, int dexterity, int health)
+        {
+            Name = name;
+            Strength = strength;
+            Intelligence = intelligence;
+            Dexterity = dexterity;
+            Health = health;
+        }
+
+        // Method to show the status of the current player
+        public void ShowStatus()
+        {
+            System.Console.WriteLine("Name: {0}", Name);
+            System.Console.WriteLine("Strength: {0}", Strength);
+            System.Console.WriteLine("Intelligence: {0}", Intelligence);
+            System.Console.WriteLine("Dexterity: {0}", Dexterity);
+            System.Console.WriteLine("Health: {0}", Health);
+        }
+
+        public void Attack(object obj)
+        {
+            Human enemy = obj as Human;
+            if(enemy == null)
+            {
+                System.Console.WriteLine("Attack failed. This player does not exist.");
+            }
+            else
+            {
+                enemy.Health -= Strength * 5;
+            }
+        }
+    }
+
+    public class Wizard : Human
+    {
+        public Wizard(string name) : base(name)
+        {
+            Intelligence = 25;
+            Health = 50;
+        }
+
+        public void Heal()
+        {
+            this.Health += Intelligence * 5;
+        }
+
+        public void Fireball(object obj)
+        {
+            Human enemy = obj as Human;
+            Random random = new Random();
+            if (enemy == null)
+            {
+                System.Console.WriteLine("Fireball failed. This player does not exist.");
+            }
+            else
+            {
+                int damage = random.Next(20, 51);
+                enemy.Health -= damage;
+            }
+        }
+    }
+
+    public class Samurai : Human
+    {
+        // private int _Samurai_Count = 0;
+        // public int Samurai_Count { 
+        //     get { return _Samurai_Count; }
+        //     set { _Samurai_Count = value; }
+        // }
+
+        // public int Samurai_Count { get; set; }
+
+        public Samurai(string name) : base(name)
+        {
+            Health = 200;
+            // Samurai_Count += 1;
+        }
+
+        // public static void How_Many()
+        // {
+        //     // object sam_properties = PropertyInfo.GetValue(this, null);
+        //     System.Console.WriteLine("Number of Samurais: {0}", numSams);
+        // }
+
+        public void Meditate()
+        {
+            this.Health = 200;
+        }
+
+        public void Death_Blow(object obj)
+        {
+            Human enemy = obj as Human;
+            if (enemy == null)
+            {
+                System.Console.WriteLine("Death_Blow failed. This player does not exist.");
+            }
+            else if (enemy.Health < 50)
+            {
+                enemy.Health = 0;
+            }
+            else
+            {
+                System.Console.WriteLine("Death_Blow failed. This player has over 50 health.");
+            }
+        }
+    }
+
+    public class Ninja : Human
+    {
+        public Ninja(string name) : base(name)
+        {
+            Dexterity = 175;
+        }
+
+        public void Get_Away()
+        {
+            this.Health -= 15;
+        }
+
+        public void Steal(object obj)
+        {
+            Human enemy = obj as Human;
+            if (enemy == null)
+            {
+                System.Console.WriteLine("Steal failed. This player does not exist.");
+            }
+            else
+            {
+                this.Attack(enemy);
+                this.Health += 10;
+            }
+        }
+    }
+
+    class Program
+    {
         static void Main(string[] args)
         {
             // Human me = new Human("Maki", 3, 10, 3, 200);
-            Human me = new Human("Maki", health: 200);
+            // Create new players
+            Human me = new Human("Maki");
+            me.Health = 200;
             Human Lola = new Human("Lola");
-            System.Console.WriteLine(me);
-            System.Console.WriteLine("--------Before Attacking--------");
+            Wizard Wanda = new Wizard("Wanda");
+            Ninja Nick = new Ninja("Nick");
+            Samurai Sam = new Samurai("Sam");
+            Samurai Ducky = new Samurai("Ducky");
+            // Show stats of players at beginning of game
+            System.Console.WriteLine("\n=====Before Attacking======");
+            System.Console.WriteLine("\n---------Player 1----------");
             me.ShowStatus();
-            System.Console.WriteLine("--------------------------------");
+            System.Console.WriteLine("\n---------Player 2----------");
             Lola.ShowStatus();
-            System.Console.WriteLine("--------After Attacking---------");
+            System.Console.WriteLine("\n---------Player 3----------");
+            Wanda.ShowStatus();
+            System.Console.WriteLine("\n---------Player 4----------");
+            Nick.ShowStatus();
+            System.Console.WriteLine("\n---------Player 5----------");
+            Sam.ShowStatus();
+            // Attack round
+            System.Console.WriteLine("\n======While Attacking======");
+            Wanda.Fireball(me);
+            System.Console.WriteLine("\n1. Wanda fireballs me.");
             me.Attack(Lola);
+            System.Console.WriteLine("\n2. I attack Lola.");
+            Lola.Attack(Wanda);
+            System.Console.WriteLine("\n3. Lola attacks Wanda.");
+            Sam.Death_Blow(Wanda);
+            System.Console.WriteLine("\n4. Sam death blows Wanda. (Suck it Wanda!)");
+            Nick.Steal(Sam);
+            System.Console.WriteLine("\n5. Nick steals from Sam.");
+            // Show stats of players after attacking
+            System.Console.WriteLine("\n======After Attacking======");
+            System.Console.WriteLine("\n---------Player 1----------");
+            me.ShowStatus();
+            System.Console.WriteLine("\n---------Player 2----------");
             Lola.ShowStatus();
-            System.Console.WriteLine("--------------------------------");
+            System.Console.WriteLine("\n---------Player 3----------");
+            Wanda.ShowStatus();
+            System.Console.WriteLine("\n---------Player 4----------");
+            Nick.ShowStatus();
+            System.Console.WriteLine("\n---------Player 5----------");
+            Sam.ShowStatus();
+            // Heal round
+            System.Console.WriteLine("\n=======While Healing=======");
+            Wanda.Heal();
+            System.Console.WriteLine("\n1. Wanda uses heal on herself.");
+            Sam.Meditate();
+            System.Console.WriteLine("\n2. Sam uses meditate on himself.");
+            System.Console.WriteLine("\n=======After Healing=======");
+            System.Console.WriteLine("\n---------Player 3----------");
+            Wanda.ShowStatus();
+            System.Console.WriteLine("\n---------Player 5----------");
+            Sam.ShowStatus();
+            System.Console.WriteLine("\n");
+            // Samurai.How_Many();
         }
     }
 }
