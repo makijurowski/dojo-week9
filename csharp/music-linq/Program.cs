606935eb75840ae59aa74e82745188cc8cb6c55e
makiroggers <makiroggers@gmail.com>
2017-10-18 18:43:29 -0700

    Somehow forgot one... now added.

--- csharp/music-linq/Program.cs
+++ csharp/music-linq/Program.cs
@@ -23,7 +23,7 @@ namespace ConsoleApplication {
                 System.Console.WriteLine ("\n========CHALLENGE 1=========");
                 System.Console.WriteLine ("Find artist from Mount Vernon");
                 System.Console.WriteLine ("----------------------------\n");
-                System.Console.WriteLine ("ARTIST NAME: {0}\nREAL NAME: {1}\nAGE: {2}", 
+                System.Console.WriteLine ("ARTIST NAME: {0}\nREAL NAME: {1}\nAGE: {2}\n", 
                                           artist.ArtistName, 
                                           artist.RealName, 
                                           artist.Age);
@@ -38,7 +38,7 @@ namespace ConsoleApplication {
             System.Console.WriteLine ("\n=========CHALLENGE 2=========");
             System.Console.WriteLine ("The youngest artist in collection");
             System.Console.WriteLine ("-----------------------------\n");
-            System.Console.WriteLine ("ARTIST NAME: {0}\nREAL NAME: {1}\nAGE: {2}", 
+            System.Console.WriteLine ("ARTIST NAME: {0}\nREAL NAME: {1}\nAGE: {2}\n", 
                                       challenge2.First().ArtistName, 
                                       challenge2.First().RealName, 
                                       challenge2.First().Age);
@@ -60,9 +60,25 @@ namespace ConsoleApplication {
                 count += 1;
             }
 
+            // 4. Display all groups with names less than 8 characters in length.
+            IEnumerable<Group> challenge4 = Groups.Where (group => group.GroupName.Length < 8);
 
-            // 4. Display the 3 oldest artist from Atlanta
-            var challenge4 = (from artist in Artists 
+            System.Console.WriteLine ("\n=========CHALLENGE 4=========");
+            System.Console.WriteLine ("Groups with names less than 8 characters");
+            System.Console.WriteLine ("-----------------------------\n");
+
+            count = 1;
+            foreach (Group group in challenge4) {
+                System.Console.WriteLine ("{0}. GROUP NAME: {1} ({2} characters)\n",
+                                          count,
+                                          group.GroupName,
+                                          group.GroupName.Length);
+                count += 1;
+            }
+
+
+            // 5. Display the 3 oldest artist from Atlanta
+            var challenge5 = (from artist in Artists 
                               where artist.Hometown.Equals ("Atlanta") 
                               orderby artist.Age descending 
                               select new {
@@ -71,12 +87,12 @@ namespace ConsoleApplication {
                                   hometown = artist.Hometown,
                                   age = artist.Age}).Take (3);
 
-            System.Console.WriteLine ("\n=========CHALLENGE 4=========");
+            System.Console.WriteLine ("\n=========CHALLENGE 5=========");
             System.Console.WriteLine ("Three oldest artists from Atlanta");
             System.Console.WriteLine ("-----------------------------\n");
 
             count = 1;
-            foreach (var artist in challenge4) {
+            foreach (var artist in challenge5) {
                 System.Console.WriteLine ("{0}. ARTIST NAME: {1}\n   REAL NAME: {2}\n   HOMETOWN: {3}\n   AGE: {4}\n", 
                                           count, 
                                           artist.artistName, 
@@ -87,17 +103,17 @@ namespace ConsoleApplication {
             }
 
 
-            // 5. (Optional) Display the Group Name of all groups that have members that are not from New York City
-            var challenge5 = (from artist in Artists 
+            // 6. (Optional) Display the Group Name of all groups that have members that are not from New York City
+            var challenge6 = (from artist in Artists 
                               where!artist.Hometown.Contains ("New York City") 
                               select artist.GroupId).Distinct ();
 
-            System.Console.WriteLine ("\n=========CHALLENGE 5=========");
+            System.Console.WriteLine ("\n=========CHALLENGE 6=========");
             System.Console.WriteLine ("Groups without NYC members");
             System.Console.WriteLine ("-----------------------------\n");
 
             count = 1;
-            foreach (var item in challenge5) {
+            foreach (var item in challenge6) {
                 var groups = from each in Groups
                 where each.Id.Equals (item)
                 select each.GroupName;
@@ -111,8 +127,8 @@ namespace ConsoleApplication {
             }
 
 
-            // 6. (Optional) Display the artist names of all members of the group 'Wu-Tang Clan'
-            var challenge6 = (from artist in Artists 
+            // 7. (Optional) Display the artist names of all members of the group 'Wu-Tang Clan'
+            var challenge7 = (from artist in Artists 
                               join groupList in Groups 
                               on artist.GroupId equals groupList.Id 
                               where groupList.GroupName == "Wu-Tang Clan"
@@ -121,12 +137,12 @@ namespace ConsoleApplication {
                                   groupName = groupList.GroupName
                               });
 
-            System.Console.WriteLine ("\n=========CHALLENGE 6=========");
+            System.Console.WriteLine ("\n=========Challenge 7=========");
             System.Console.WriteLine ("All Wu-Tang Clan members");
             System.Console.WriteLine ("-----------------------------\n");
 
             count = 1;
-            foreach (var artist in challenge6) {
+            foreach (var artist in challenge7) {
                 System.Console.WriteLine ("{0}. ARTIST NAME: {1} (from {2})\n", 
                                           count, 
                                           artist.artistName, 
