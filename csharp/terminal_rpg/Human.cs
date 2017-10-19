653dc60a1f1923cbc7719cc7501ac1f4df5a85e7
makiroggers <makiroggers@gmail.com>
2017-10-18 21:54:37 -0700

    Updated formatting

--- csharp/terminal_rpg/Human.cs
+++ csharp/terminal_rpg/Human.cs
@@ -1,8 +1,10 @@
 using System;
 
-namespace terminal_rpg {
+namespace terminal_rpg
+{
     // Base parent class
-    public class Human {
+    public class Human
+    {
         // Properties for Human class
         public string Name { get; set; }
         public int Strength { get; set; }
@@ -11,7 +13,8 @@ namespace terminal_rpg {
         public int Health { get; set; }
 
         // Constructor for Human class with default values added (only passes name as argument)
-        public Human (string name) {
+        public Human(string name)
+        {
             Name = name;
             Strength = 3;
             Intelligence = 3;
@@ -20,7 +23,8 @@ namespace terminal_rpg {
         }
 
         // Constructor for Human class without default values (passes all stats as arguments)
-        public Human (string name, int strength, int intelligence, int dexterity, int health) {
+        public Human(string name, int strength, int intelligence, int dexterity, int health)
+        {
             Name = name;
             Strength = strength;
             Intelligence = intelligence;
@@ -29,20 +33,25 @@ namespace terminal_rpg {
         }
 
         // Method to show the status of the current player
-        public void ShowStatus () {
-            System.Console.WriteLine ("Name: {0}", Name);
-            System.Console.WriteLine ("Strength: {0}", Strength);
-            System.Console.WriteLine ("Intelligence: {0}", Intelligence);
-            System.Console.WriteLine ("Dexterity: {0}", Dexterity);
-            System.Console.WriteLine ("Health: {0}", Health);
+        public void ShowStatus()
+        {
+            System.Console.WriteLine("Name: {0}", Name);
+            System.Console.WriteLine("Strength: {0}", Strength);
+            System.Console.WriteLine("Intelligence: {0}", Intelligence);
+            System.Console.WriteLine("Dexterity: {0}", Dexterity);
+            System.Console.WriteLine("Health: {0}", Health);
         }
 
         // Method used to attack another player passed by reference
-        public void Attack (object obj) {
+        public void Attack(object obj)
+        {
             Human enemy = obj as Human;
-            if (enemy == null) {
-                System.Console.WriteLine ("Attack failed. This player does not exist.");
-            } else {
+            if (enemy == null)
+            {
+                System.Console.WriteLine("Attack failed. This player does not exist.");
+            }
+            else
+            {
                 enemy.Health -= Strength * 5;
             }
         }
--- csharp/terminal_rpg/Program.cs
+++ csharp/terminal_rpg/Program.cs
@@ -1,70 +1,78 @@
 ﻿using System;
 
-namespace terminal_rpg {
-    class Program {
+namespace terminal_rpg
+{
+    class Program
+    {
         // Main method invoked upon running program
-        static void Main (string[] args) {
+        static void Main(string[] args)
+        {
             // Create new players
-            Human me = new Human ("Maki");
+            Human me = new Human("Maki");
             me.Health = 200;
-            Human Lola = new Human ("Lola");
-            Wizard Wanda = new Wizard ("Wanda");
-            Ninja Nick = new Ninja ("Nick");
-            Samurai Sam = new Samurai ("Sam");
-            Samurai Ducky = new Samurai ("Ducky");
+            Human Lola = new Human("Lola");
+            Wizard Wanda = new Wizard("Wanda");
+            Ninja Nick = new Ninja("Nick");
+            Samurai Sam = new Samurai("Sam");
+            Samurai Ducky = new Samurai("Ducky");
+            Enemy Steve = new Enemy("Steve");
 
             // Show stats of players at beginning of game
-            System.Console.WriteLine ("\n=====Before Attacking======");
-            System.Console.WriteLine ("\n---------Player 1----------");
-            me.ShowStatus ();
-            System.Console.WriteLine ("\n---------Player 2----------");
-            Lola.ShowStatus ();
-            System.Console.WriteLine ("\n---------Player 3----------");
-            Wanda.ShowStatus ();
-            System.Console.WriteLine ("\n---------Player 4----------");
-            Nick.ShowStatus ();
-            System.Console.WriteLine ("\n---------Player 5----------");
-            Sam.ShowStatus ();
+            System.Console.WriteLine("\n=====Before Attacking======");
+            System.Console.WriteLine("\n---------Player 1----------");
+            me.ShowStatus();
+            System.Console.WriteLine("\n---------Player 2----------");
+            Lola.ShowStatus();
+            System.Console.WriteLine("\n---------Player 3----------");
+            Wanda.ShowStatus();
+            System.Console.WriteLine("\n---------Player 4----------");
+            Nick.ShowStatus();
+            System.Console.WriteLine("\n---------Player 5----------");
+            Sam.ShowStatus();
+            System.Console.WriteLine("\n---------Enemy 1-----------");
+            Steve.ShowStatus();
 
             // Attack round
-            System.Console.WriteLine ("\n======While Attacking======");
-            Wanda.Fireball (me);
-            System.Console.WriteLine ("\n1. Wanda fireballs me.");
-            me.Attack (Lola);
-            System.Console.WriteLine ("\n2. I attack Lola.");
-            Lola.Attack (Wanda);
-            System.Console.WriteLine ("\n3. Lola attacks Wanda.");
-            Sam.Death_Blow (Wanda);
-            System.Console.WriteLine ("\n4. Sam death blows Wanda. (Suck it Wanda!)");
-            Nick.Steal (Sam);
-            System.Console.WriteLine ("\n5. Nick steals from Sam.");
+            System.Console.WriteLine("\n======While Attacking======");
+            Wanda.Fireball(me);
+            System.Console.WriteLine("\n1. Wanda fireballs me.");
+            me.Attack(Lola);
+            System.Console.WriteLine("\n2. I attack Lola.");
+            Lola.Attack(Wanda);
+            System.Console.WriteLine("\n3. Lola attacks Wanda.");
+            Sam.Death_Blow(Wanda);
+            System.Console.WriteLine("\n4. Sam death blows Wanda. (Suck it Wanda!)");
+            Nick.Steal(Sam);
+            System.Console.WriteLine("\n5. Nick steals from Sam.");
 
             // Show stats of players after attacking
-            System.Console.WriteLine ("\n======After Attacking======");
-            System.Console.WriteLine ("\n---------Player 1----------");
-            me.ShowStatus ();
-            System.Console.WriteLine ("\n---------Player 2----------");
-            Lola.ShowStatus ();
-            System.Console.WriteLine ("\n---------Player 3----------");
-            Wanda.ShowStatus ();
-            System.Console.WriteLine ("\n---------Player 4----------");
-            Nick.ShowStatus ();
-            System.Console.WriteLine ("\n---------Player 5----------");
-            Sam.ShowStatus ();
+            System.Console.WriteLine("\n======After Attacking======");
+            System.Console.WriteLine("\n---------Player 1----------");
+            me.ShowStatus();
+            System.Console.WriteLine("\n---------Player 2----------");
+            Lola.ShowStatus();
+            System.Console.WriteLine("\n---------Player 3----------");
+            Wanda.ShowStatus();
+            System.Console.WriteLine("\n---------Player 4----------");
+            Nick.ShowStatus();
+            System.Console.WriteLine("\n---------Player 5----------");
+            Sam.ShowStatus();
 
             // Heal round
-            System.Console.WriteLine ("\n=======While Healing=======");
-            Wanda.Heal ();
-            System.Console.WriteLine ("\n1. Wanda uses heal on herself.");
-            Sam.Meditate ();
-            System.Console.WriteLine ("\n2. Sam uses meditate on himself.");
-            System.Console.WriteLine ("\n=======After Healing=======");
-            System.Console.WriteLine ("\n---------Player 3----------");
-            Wanda.ShowStatus ();
-            System.Console.WriteLine ("\n---------Player 5----------");
-            Sam.ShowStatus ();
-            System.Console.WriteLine ("\n");
-            Samurai.How_Many ();
+            System.Console.WriteLine("\n=======While Healing=======");
+            Wanda.Heal();
+            System.Console.WriteLine("\n1. Wanda uses heal on herself.");
+            Sam.Meditate();
+            System.Console.WriteLine("\n2. Sam uses meditate on himself.");
+
+            // Show stats of players after healing
+            System.Console.WriteLine("\n=======After Healing=======");
+            System.Console.WriteLine("\n---------Player 3----------");
+            Wanda.ShowStatus();
+            System.Console.WriteLine("\n---------Player 5----------");
+            Sam.ShowStatus();
+            System.Console.WriteLine("\n");
+            Samurai.How_Many();
         }
     }
 }
